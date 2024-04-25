964154479285e8b8f773d4a6641009ea291b6219
kirangond <kirangond05@gmail.com>
2024-04-25 19:53:20 +0530

    packages

--- src/com/p1/first/A.java
+++ src/com/p1/first/A.java
@@ -1,19 +1,17 @@
 package com.p1.first;
 
 public class A {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		first();
-
-
-	}
-
-	private static void first() {
+	public void first() {
 		// TODO Auto-generated method stub
 		System.out.println("method from package 1");
 	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+
 
 
 }
---
+++ src/com/p2/second/B.java
@@ -0,0 +1,13 @@
+package com.p2.second;
+
+import com.p1.first.A;
+
+public class B extends A{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         A a = new A();
+         a.first();
+	}
+
+}
