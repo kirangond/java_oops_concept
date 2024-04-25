b057a535071cd0b7e0b953184d905060b05675d1
kirangond <kirangond05@gmail.com>
2024-04-25 19:38:03 +0530

    package

---
+++ src/com/p1/first/A.java
@@ -0,0 +1,19 @@
+package com.p1.first;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		first();
+
+
+	}
+
+	private static void first() {
+		// TODO Auto-generated method stub
+		System.out.println("method from package 1");
+	}
+
+
+}
