ba36506c944a2b0d271499ebe974e3bcbf69d0d9
kirangond <kirangond05@gmail.com>
2024-05-06 22:49:29 +0530

    exception

--- src/com/exceptions/A.java
+++ src/com/exceptions/A.java
@@ -1,19 +1,27 @@
 package com.exceptions;
 
 public class A {
+	
+	static String error_message="Execption occured halt it: ";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		try {
 			int x = 10;
-			int y = 20;
+			int y = 0;
 			int z = x/y;
 			System.out.println(100);
-		} catch (Exception e) {
+			
+		}catch (ArithmeticException e) {
 			// TODO: handle exception
 			System.out.println(e);
-			e.printStackTrace();
+			
+		} 
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("we will get exception: " + error_message + e);
+			//e.printStackTrace();
 		}
 
 	}
