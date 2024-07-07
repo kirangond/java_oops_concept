66d8c3b4779f66971cd44392aa3c2284daa56730
kirangond <kirangond05@gmail.com>
2024-07-07 22:39:57 +0530

    super

---
+++ src/com/inheritance/B5.java
@@ -0,0 +1,12 @@
+package com.inheritance;
+
+public class B5 extends A5{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       B5 b = new B5();
+       System.out.println(A5.k);
+       System.out.println(b.k);
+	}
+
+}
